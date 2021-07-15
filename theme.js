a16be5fa25003f7ad0911f7a615b292f40b3ec9c
Ritika Bihari <73096030+Ritika42894@users.noreply.github.com>
2021-07-15 14:37:53 +0530

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,18 @@
+export const theme = Object.freeze({
+  fontFamily: 'Roboto,Open Sans',
+  colorScheme: 'light',
+  // colors: {
+  //   'deep-blue': ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
+  //   blue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
+  // },
+  // shadows: {
+  //   md: '1px 1px 3px rgba(0,0,0,.25)',
+  //   xl: '5px 5px 3px rgba(0,0,0,.25)',
+  // },
+  headings: {
+    fontFamily: 'Roboto, sans-serif',
+    sizes: {
+      h1: { fontSize: 30 },
+    },
+  },
+});
